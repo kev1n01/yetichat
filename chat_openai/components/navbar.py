0fb3a2407a6e7f32cbf33bbd618bcb0b9bcf62ba
kevin arnold flores pacheco <arnold_k2001@hotmail.com>
2023-12-04 13:03:52 -0500

    theming added

--- chat_openai/components/navbar.py
+++ chat_openai/components/navbar.py
@@ -1,11 +1,23 @@
 import reflex as rx
 
 def navbar() -> rx.Component:
-    return rx.vstack(
+    return rx.container(
         rx.box(
             rx.text(
                 "Chef Yeti",
-                class_name="font-bold text-2xl text-white mt-4 p-1 justify-center",
+                class_name="font-bold text-2xl text-blue p-1  mt-3",
             ),
-        )
+        ),
+        rx.box(
+            rx.button(
+                rx.cond(
+                    rx.color_mode == 'light',
+                    rx.icon(tag="sun"),
+                    rx.icon(tag="moon"),
+                ),
+                on_click=rx.toggle_color_mode,
+                class_name="p-1 mt-3",
+            ), 
+        ),
+        class_name="flex justify-between",
     )
